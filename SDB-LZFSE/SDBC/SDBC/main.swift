ce2c771c4ab058ae4dcf4f04fde0e3cd8770a60c
Scott D. Bowen <49947173+scott-d-bowen@users.noreply.github.com>
2021-08-25 22:12:07 +1000

    Initial commit.

---
+++ SDB-LZFSE/SDBC/SDBC/main.swift
@@ -0,0 +1,94 @@
+//
+//  main.swift
+//  SDBC
+//
+//  Created by Scott D. Bowen on 25/8/21.
+//
+
+import Foundation
+import DataCompression
+
+var GLOBAL_START = Date()
+print("SDBC (SMT LZFSE Compressor):")
+let dispatchGroup = DispatchGroup()
+let MICRO_CHUNK_SIZE = 1 * 1024 * 1024
+
+extension Array {
+    func chunked(into size: Int) -> [[Element]] {
+        return stride(from: 0, to: count, by: size).map {
+            Array(self[$0 ..< Swift.min($0 + size, count)])
+        }
+    }
+}
+extension Data {
+
+    init<T>(from value: T) {
+        self = Swift.withUnsafeBytes(of: value) { Data($0) }
+    }
+
+    func to<T>(type: T.Type) -> T? where T: ExpressibleByIntegerLiteral {
+        var value: T = 0
+        guard count >= MemoryLayout.size(ofValue: value) else { return nil }
+        _ = Swift.withUnsafeMutableBytes(of: &value, { copyBytes(to: $0)} )
+        return value
+    }
+}
+func benchmarkCode(text: String) {
+    let value = -GLOBAL_START.timeIntervalSinceNow
+    print("\(Int64(value * 1_000_000)) nanoseconds \(text)")
+    GLOBAL_START = Date()
+}
+
+// Compression Algorithm Test:
+
+let inputFileURL:  URL = URL(fileURLWithPath: "/Users/sdb/Testing/Xcode13-beta5.tar")
+let outputFileURL: URL = URL(fileURLWithPath: "/Users/sdb/Testing/Xcode13-beta5.sdb")
+let fm = FileManager()
+fm.createFile(atPath: outputFileURL.path, contents: Data(), attributes: nil)
+let handle = try FileHandle.init(forWritingTo: outputFileURL)
+var chunks: [Data] = [Data(), Data(), Data(), Data(), Data(), Data(), Data(), Data(),
+                      Data(), Data(), Data(), Data(), Data(), Data(), Data(), Data()]
+
+if let stream = InputStream(url: inputFileURL) {
+    dispatchGroup.enter()
+    Task {
+        var buf = [UInt8](repeating: 0, count: 16 * MICRO_CHUNK_SIZE)
+
+        stream.open()
+        while case let amount = stream.read(&buf, maxLength: 16 * MICRO_CHUNK_SIZE), amount > 0 {
+            
+            let splicedBuffer = (buf.chunked(into: MICRO_CHUNK_SIZE))
+            
+            await withTaskGroup(of: (Int, Crc32, Data).self) { group in
+                
+                for i in 0..<16 {
+                    group.addTask {
+                        chunks[i] = Data(splicedBuffer[i]).compress(withAlgorithm: .lzfse)!
+                        return (i, chunks[i].crc32(), chunks[i])
+                    }
+                }
+                for await triple in group {
+                    // print(triple.0, triple.1)
+                    chunks[triple.0] = triple.2
+                }
+                // let handle = try FileHandle.init(forWritingAtPath: outputFileURL.absoluteString)
+                for i in 0..<16 {
+                    let chunkSize = UInt32(chunks[i].count)
+                    try? handle.write(contentsOf: Data(from: chunkSize) )
+                    try? handle.write(contentsOf: chunks[i]);
+                    chunks[i] = Data(repeating: 0x00, count: 16)
+                }
+                print(".", terminator: "")
+            }
+        }
+        dispatchGroup.leave()
+        print()
+    }
+    stream.close()
+    dispatchGroup.wait()
+}
+benchmarkCode(text: "Compression of: Xcode13-beta5.tar (to: .sdb) complete.")
+
+
+print("Goodbye.")
+sleep(3600)
---
+++ SDB-LZFSE/SDBX/SDBX/SDBX/main.swift
@@ -0,0 +1,91 @@
+//
+//  main.swift
+//  SDBX
+//
+//  Created by Scott D. Bowen on 25/8/21.
+//
+
+import Foundation
+import DataCompression
+
+var GLOBAL_START = Date()
+print("SDBX (LZFSE Decompressor):")
+let dispatchGroup = DispatchGroup()
+let MICRO_CHUNK_SIZE = 1 * 1024 * 1024
+
+enum InputStreamError: Error {
+    case invalidData
+}
+extension Array {
+    func chunked(into size: Int) -> [[Element]] {
+        return stride(from: 0, to: count, by: size).map {
+            Array(self[$0 ..< Swift.min($0 + size, count)])
+        }
+    }
+}
+extension Data {
+    
+    init<T>(from value: T) {
+        self = Swift.withUnsafeBytes(of: value) { Data($0) }
+    }
+    
+    func to<T>(type: T.Type) -> T? where T: ExpressibleByIntegerLiteral {
+        var value: T = 0 // as! T
+        guard count >= MemoryLayout.size(ofValue: value) else { return nil }
+        _ = Swift.withUnsafeMutableBytes(of: &value, { copyBytes(to: $0)} )
+        return value
+    }
+}
+func benchmarkCode(text: String) {
+    let value = -GLOBAL_START.timeIntervalSinceNow
+    print("\(Int64(value * 1_000_000)) nanoseconds \(text)")
+    GLOBAL_START = Date()
+}
+
+// Compression Algorithm Test:
+
+let inputFileURL:  URL = URL(fileURLWithPath: "/Users/sdb/Testing/Xcode13-beta5.sdb")
+let outputFileURL: URL = URL(fileURLWithPath: "/Users/sdb/Testing/Xcode13-beta5.tar.extracted")
+let fm = FileManager()
+fm.createFile(atPath: outputFileURL.path, contents: Data(), attributes: nil)
+let handle = try FileHandle.init(forWritingTo: outputFileURL)
+var chunk: Data = Data()
+
+if let stream = InputStream(url: inputFileURL) {
+    dispatchGroup.enter()
+    
+    var bufA = [UInt8](repeating: 0, count: 4)
+    var bufB = [UInt8](repeating: 0, count: 2 * MICRO_CHUNK_SIZE)
+    
+    stream.open()
+    var chunkSizeA = stream.read(&bufA, maxLength: 4)
+    guard (chunkSizeA == 4) else {
+        print(" * Error decompressig data input stream.")
+        throw InputStreamError.invalidData
+    }
+    var chunkSizeB = Data(bufA).to(type: UInt32.self)
+    
+    var iteration = 1
+    while case let amount = stream.read(&bufB, maxLength: Int(chunkSizeB!)), amount > 0 {
+        
+        chunk = Data(bufB).decompress(withAlgorithm: .lzfse)!
+        if (chunk.count != 1024*1024) {
+            print(iteration, chunk.count)
+        }
+        try? handle.write(contentsOf: chunk);
+        // chunk = Data(repeating: 0x00, count: 16)
+        // print(".", terminator: "")
+        chunkSizeA = stream.read(&bufA, maxLength: 4)
+        chunkSizeB = Data(bufA).to(type: UInt32.self)
+        
+        iteration += 1
+    }
+    dispatchGroup.leave()
+    stream.close()
+    try handle.close()
+    dispatchGroup.wait()
+}
+benchmarkCode(text: "Decompression of: Xcode13-beta5.tar.extracted (from: .sdb) complete.")
+
+print("Goodbye.")
+sleep(3600)
